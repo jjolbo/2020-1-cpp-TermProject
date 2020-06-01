de25eeb115a94d27aec23fddf99ff691667fad8d
jjolbo <jeongha3751@kookmin.ac.kr>
2020-06-02 00:01:43 +0900

    refactor: main 수정

--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
-#include "main.h"
-#include <iostream>
-using namespace std;
-#define LEVEL 11;
+#include "snake.h"
+#include "iostream"
 
 int main()
 {
-  // 0 : USER
-  MODE command = init_game();
-
-  if (command == USER)
-  {
-    cout << 1 << endl;
-    // user_mode_game(LEVEL);
-  }
+  snakeclass s;
+  s.start();
 
   return 0;
 }
